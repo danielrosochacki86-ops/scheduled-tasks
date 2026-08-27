86292a285f54ccb4515575da8509767e1345047a
danielrosochacki86-ops <daniel.rosochacki.86@gmail.com>
2026-08-27 15:43:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def send_wishes(name, receiver_email):
     #     wishes.write(data)
 
     # --- your credentials ---
-    sender_email = os.environ.get("SENDER_EMAIL")
-    sender_password = os.environ.get("SENDER_PASSWORD")
+    sender_email = os.environ.get("MY_EMAIL")
+    sender_password = os.environ.get("MY_PASSWORD")
     receiver_email = receiver_email
 
     # --- build the message ---
